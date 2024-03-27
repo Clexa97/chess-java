cf9e15ff9b8caaeb0673ae5ee0e975f042f0251d
Juliana Ransani <juliana.ransani.2@gmail.com>
2024-03-27 07:56:40 -0300

    criando as classes de posição peça e tabuleiro

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,9 +1,13 @@
 package app;
 
+import boardgame.Board;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("o inicio do xadrez");
+		
+		Board board = new Board(8, 8);
+		
 
 	}
 
